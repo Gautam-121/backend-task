09b04c030016c1abd34fc6ba5234a3d208263c47
Gautam-121 <gautamdhakate1234@gmail.com>
2023-06-30 19:26:16 +0530

    task

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,12 @@ app.use(express.json())
 //import Router
 const customerRouter = require("./route/customerRouter.js")
 
+app.get("/getData" , (req , res) => {
+    res.status(200).json({
+        message : "all Done"
+    })
+})
+
 app.use("/api/v1" , customerRouter)
 
 //import errorMiddleware
--- backend/config/mongooseConnection.js
+++ backend/config/mongooseConnection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connection = ()=>{
 
-    mongoose.connect( process.env.DB_URL , {
+    mongoose.connect(process.env.DB_URL , {
         useNewUrlParser : true
     })
     .then(data => console.log("mongoDB is connected on" , data.connection.host))
